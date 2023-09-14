35026d9d95fc0d8558d2726be43d571d58c0a8ea
khaledsaifulla05 <khaledsaifulla2001@gmail.com>
2023-09-14 21:52:10 +0600

    added fake data & data load

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Courses from './Components/Header/Courses/Courses'
+import Header from './Components/Header/Header'
+function App() {
+  
+
+  return (
+    <>
+      
+     {/* Header */}
+     <Header></Header>
+
+     {/* Courses */}
+
+     <Courses></Courses>
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Courses/Courses.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const Courses = () => {
+    const [courses, setCourses] = useState([]);
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Courses;
